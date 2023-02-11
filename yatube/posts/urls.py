d49bb8407f1ff2093bce342f3b446fb1eb9bfe3c
Daniil Voronov <d.a.voronov550@gmail.com>
2023-02-11 20:27:44 +0300

    Added namespaces

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
     path('', views.index),
     path('index/', views.index, name='index'),
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,9 +3,17 @@ from django.shortcuts import render
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    title = 'Это главная страница проекта Yatube'
+    context = {
+        'title': title,
+    }
+    return render(request, template, context)
 
 
 def group_list(request):
     template = 'posts/group_list.html'
-    return render(request, template)
+    title = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'title': title,
+    }
+    return render(request, template, context)
