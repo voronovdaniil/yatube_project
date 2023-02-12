82a68e6c24cd2f424411de94eacfd89c5f36db57
Daniil Voronov <d.a.voronov550@gmail.com>
2023-02-12 16:54:12 +0300

    Updated admin.py in posts

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
 # Register your models here.
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    # Перечисляем поля, которые должны отображаться в админке
+    list_display = ('pk', 'text', 'pub_date', 'author')
+    # Добавляем интерфейс для поиска по тексту постов
+    search_fields = ('text',)
+    # Добавляем возможность фильтрации по дате
+    list_filter = ('pub_date',)
+    # Это свойство сработает для всех колонок: где пусто — там будет эта строка
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Post, PostAdmin)
